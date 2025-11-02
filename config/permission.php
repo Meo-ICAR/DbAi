1b4824d28dadd48daffcd116980cc3469d18690b
Meo-ICAR <piergiuseppe.meo@icar.cnr.it>
2025-11-02 15:49:43 +0100

    roles

--- config/permission.php
+++ config/permission.php
@@ -12,8 +12,7 @@ return [
          * The model you want to use as a Permission model needs to implement the
          * `Spatie\Permission\Contracts\Permission` contract.
          */
-
-        'permission' => Spatie\Permission\Models\Permission::class,
+        'permission' => App\Models\Permission::class,
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
@@ -24,7 +23,7 @@ return [
          * `Spatie\Permission\Contracts\Role` contract.
          */
 
-        'role' => Spatie\Permission\Models\Role::class,
+        'role' => App\Models\Role::class,
 
     ],
 
